39c613552174ace90a25525aed0ad17316228929
Sha-beny <125878878+Sha-beny@users.noreply.github.com>
2023-04-23 15:31:57 +0100

    Update utils.c
    utils.c

--- utils.c
+++ utils.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * is_printable - Evaluates if a char is printable
+ * is_printable - Evaluates all if a char is printable
  * @c: Char to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
@@ -16,7 +16,7 @@ int is_printable(char c)
 
 /**
  * append_hexa_code - Append ascci in hexadecimal code to buffer
- * @buffer: Array of chars.
+ * @buffer: Array of the chars.
  * @i: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
@@ -24,7 +24,7 @@ int is_printable(char c)
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
-	/* The hexa format code is always 2 digits long */
+	/* The hexa format  code should always be 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
