eeb6da0a91c6924f829c6b155f6742a6aa8fb11a
Nzokapeter00 <125442213+BIGNATIONORG@users.noreply.github.com>
2023-04-24 14:02:00 +0300

    Update get_size.c
    new get_size.c

--- get_size.c
+++ get_size.c
@@ -3,7 +3,7 @@
 /**
  * get_size - Calculates the size to cast the every argument
  * @format: Formatted string in which to print all the arguments
- * @i: List of all arguments to be printed.
+ * @i: List of arguments to be printed.
  *
  * Return: Precision.
  */
